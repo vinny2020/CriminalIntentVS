b45f48236af2d2a6d7210312d6dc71cb4c257a4d
Vincent Stoessel <vincents@backbase.com>
2020-08-01 03:23:08 -0400

    Dialogs, Starting on menu items

--- app/src/main/java/com/xaymaca/criminalintent/controller/CrimeFragment.kt
+++ app/src/main/java/com/xaymaca/criminalintent/controller/CrimeFragment.kt
@@ -20,10 +20,12 @@ import androidx.lifecycle.Observer
 
 private const val ARG_CRIME_ID = "crime_id"
 private const val TAG = "CrimeFragment"
+private const val DIALOG_DATE = "DialogDate"
+private const val REQUEST_DATE = 0
 
 
 
-class CrimeFragment : Fragment() {
+class CrimeFragment : Fragment(), DatePickerFragment.Callbacks {
 
     private lateinit var crime: Crime
     private lateinit var titleField: EditText
@@ -53,10 +55,7 @@ class CrimeFragment : Fragment() {
         dateButton = view.findViewById(R.id.crime_date) as Button
         solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
 
-        dateButton.apply {
-            text = crime.date.toString()
-            isEnabled = false
-        }
+
 
 
 
@@ -120,6 +119,13 @@ class CrimeFragment : Fragment() {
                 crime.isSolved = isChecked
             }
         }
+
+        dateButton.setOnClickListener {
+            DatePickerFragment.newInstance(crime.date).apply {
+                setTargetFragment(this@CrimeFragment, REQUEST_DATE)
+                show(this@CrimeFragment.requireFragmentManager(), DIALOG_DATE)
+            }
+        }
     }
 
     override fun onStop() {
@@ -128,7 +134,10 @@ class CrimeFragment : Fragment() {
     }
 
 
-
+    override fun onDateSelected(date: Date ) {
+        crime.date = date
+        updateUI()
+    }
 
 
     companion object {
